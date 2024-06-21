a859be440b9996678ff98635a8a88054dc181ccb
k0ntoner <andrew4kot@gmail.com>
2024-06-21 13:07:35 +0300

    Createm debug branch

--- train/train/Program.cs
+++ train/train/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("debug");
+    }
+}
